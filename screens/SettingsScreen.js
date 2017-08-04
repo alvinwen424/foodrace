5cd8a9fbfff0dcd046d7652ec68d2606584ced09
Alvin Wen <alvinwen424@gmail.com>
2017-08-04 15:52:50 -0400

    targetmarker updates

--- screens/SettingsScreen.js
+++ screens/SettingsScreen.js
@@ -18,6 +18,15 @@ const ASPECT_RATIO = width / height
 const LATITUDE_DELTA = 0.0922
 const LONGITUDE_DELTA = LATITUDE_DELTA * ASPECT_RATIO
 
+function inRadius(marker, position){
+  if (position.latitude + .002 > marker.latitude && position.latitude - .002 < marker.latitude){
+    if(position.longitude + .002 > marker.longitude && position.longitude - .002 < marker.longitude){
+      return true;
+    }
+  }
+  return false;
+}
+
 export default class viewMap extends Component {
     constructor(props){
         super(props)
@@ -31,7 +40,12 @@ export default class viewMap extends Component {
             markerPosition: {
                 latitude: 0,
                 longitude: 0,
-            }
+            },
+            targetPosition:{
+              latitude: 40.7050, 
+              longitude: -74.0089
+            },
+            lines: []
         }
     }
 
@@ -43,14 +57,13 @@ export default class viewMap extends Component {
         var long = parseFloat(position.coords.longitude)
 
         var initialRegion = {
-            latitude: 0,
-            longitude: 0,
+            latitude: lat,
+            longitude: long,
             latitudeDelta: LATITUDE_DELTA,
             longitudeDelta: LONGITUDE_DELTA,
         }
 
-        this.setState({initialPosition: initialRegion})
-        this.setState({markerPosition: initialRegion})
+        this.setState({initialPosition: initialRegion, markerPosition: initialRegion})
     },
     (error) => alert(JSON.stringify(error)),
     {enableHighAccuracy: true, timeout: 20000, maxiumumAge: 1000 })
@@ -65,9 +78,13 @@ export default class viewMap extends Component {
           longitudeDelta: LONGITUDE_DELTA,
           latitudeDelta: LATITUDE_DELTA
         }
+        const nextState = {initialPosition: lastRegion, markerPosition: lastRegion};
+        if (inRadius(this.state.targetPosition, lastRegion)){
+          nextState.lines = [...this.state.lines, this.state.targetPosition];
+          nextState.targetPosition = {latitude: 40.7090, longitude: -74.0040}; 
+        }
 
-        this.setState({initialPosition: lastRegion})
-        this.setState({markerPosition: lastRegion})
+        this.setState(nextState)
     })
   }
 
@@ -81,6 +98,9 @@ export default class viewMap extends Component {
                 <MapView
                     style={styles.map}
                     region={this.state.initialPosition}>
+                    <MapView.Marker
+                        coordinate={this.state.targetPosition}>
+                    </MapView.Marker>
 
                     <MapView.Marker
                         coordinate={this.state.markerPosition}>
@@ -102,7 +122,6 @@ const styles = StyleSheet.create({
         width: 50,
         borderRadius: 50 / 2,
         overflow: 'hidden',
-        backgroundColor: 'red',
         borderWidth: 1,
         borderColor: 'black',
         alignItems: 'center',
@@ -131,3 +150,21 @@ const styles = StyleSheet.create({
       backgroundColor: '#F5FCFF'
     },
 });
+
+/*
+                    <MapView.Polygon
+                        coordinates={[
+                          {latitude: 40.7070, longitude: -74.0117},
+                          {latitude: 40.7058, longitude: -74.0040},
+                          {latitude: 40.7078, longitude: -74.0028},
+                        ]}
+                        //instead of erasing the line, generate the line by having them walk to specific coordinates
+                        strokeColor='red'
+                        strokeWidth={5}
+                    />*/
+
+// drop marker
+//if geolocation hits marker, draw line from original location to marker
+//
+//erase marker
+//pop up new marker
